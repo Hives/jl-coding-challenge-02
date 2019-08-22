7eb6e5de0e540dd91b451cbf8f25902a83a7b6b9
Paul Martin <paul.y.martin@johnlewis.co.uk>
2019-08-22 18:08:07 +0100

    Implement recursive filtering method

--- src/main/kotlin/fixPriceLabel.kt
+++ src/main/kotlin/fixPriceLabel.kt
@@ -1,1 +1,34 @@
-fun fixPriceLabel(input: String): String = input
+fun fixPriceLabel(label: String): String =
+    label.getPrices()
+        .pairWithFloatRepresentation()
+        .filterPrices()
+        .formatToString()
+
+private fun String.getPrices() =
+    this.split(",")
+        .map { it.split("£").last() }
+
+private fun List<String>.pairWithFloatRepresentation() =
+    this.map { Price(it, it.toFloat()) }
+
+private tailrec fun List<Price>.filterPrices(index: Int = 1): List<Price> {
+    if (index == this.size) return this
+
+    if (this[index].floatValue > this[index - 1].floatValue) {
+        return this.filterIndexed { i, _ -> i != index }.filterPrices(index)
+    } else {
+        return this.filterPrices(index + 1)
+    }
+}
+
+private fun List<Price>.formatToString(): String =
+    this.mapIndexed { index, price ->
+        when (index) {
+            0 -> "Was £${price.stringValue}"
+            this.size - 1 -> "now £${price.stringValue}"
+            else -> "then £${price.stringValue}"
+        }
+    }
+        .joinToString()
+
+data class Price(val stringValue: String, val floatValue: Float)
--- src/test/kotlin/FixPriceLabelKtTest.kt
+++ src/test/kotlin/FixPriceLabelKtTest.kt
@@ -4,7 +4,24 @@ import org.junit.jupiter.api.Test
 internal class FixPriceLabelKtTest {
     @Test
     fun `a valid label is unchanged`() {
-        val label = "Was £11, now £6”."
+        val label = "Was £11, then £9, now £6"
         assertThat(fixPriceLabel(label)).isEqualTo(label)
     }
+
+    @Test
+    fun `three prices, the middle one invalid`() {
+        val label = "Was £11, then £12, now £6"
+        assertThat(fixPriceLabel(label)).isEqualTo("Was £11, now £6")
+    }
+
+//    @Test
+//    fun `acceptance test 1`() {
+//        val label = "Was £10, then £8, then £11, now £6"
+//        assertThat(fixPriceLabel(label)).isEqualTo("was £11, now £6")
+//    }
+
 }
+
+//“Was £10, then £8, then £8, now £6”. This should be “Was £10, then £8, now £6”
+//
+//“Was £10, then £6, then £4, now £8”. This should be “Was £10, now £8”.
